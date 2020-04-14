caca2b24bb5f77859ec9739cc07a7871d8712b14
Gabriel ribeiro <gabrelvital49@gmail.com>
2020-04-14 17:20:22 -0300

    Efetuado o ajuste na sessão do usuário

--- PI_4/src/main/java/Servlet/Logout.java
+++ PI_4/src/main/java/Servlet/Logout.java
@@ -5,8 +5,12 @@
  */
 package Servlets;
 
+import Classes.Cliente;
+import Classes.ImagemProduto;
+import Classes.Produto;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -21,15 +25,51 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "Logout", urlPatterns = {"/Logout"})
 public class Logout extends HttpServlet {
 
-   private void sair(HttpServletRequest request, HttpServletResponse response)
+    private void sair(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-            HttpSession sessao = request.getSession();
-            sessao.invalidate();
-            sessao = request.getSession(true);
-            sessao.setAttribute("msg", true);
-            request.getRequestDispatcher("/WEB-INF/Index.jsp")
-                        .forward(request, response);
+        HttpSession sessao = request.getSession();
+        sessao.invalidate();
+        sessao = request.getSession(true);
+        sessao.setAttribute("msg", true);
+        Cliente c = (Cliente) sessao.getAttribute("usuarioLogado");
+        if (c != null) {
+            request.setAttribute("NomeLogadoAtt", c.getNome());
+        } else {
+            request.setAttribute("NomeLogadoAtt", "false");
+        }
+        ArrayList<Produto> listaProd = new Controller.ControllerListarProduto().getProdutosCliente();
+        ArrayList<ImagemProduto> listaImagens = new Controller.ControllerListarProduto().getImagensTotal();
+        ArrayList<ImagemProduto> listaPrimeiraImagem = new ArrayList<>();
+        for (ImagemProduto img : listaImagens) {
+            int IdProd = img.getIdProd();
+            if (listaPrimeiraImagem.isEmpty()) {
+                listaPrimeiraImagem.add(img);
+            } else {
+                int validar = 0;
+                for (ImagemProduto imgFirst : listaPrimeiraImagem) {
+                    if (IdProd == imgFirst.getIdProd()) {
+                        validar++;
+                    }
+                }
+                if (validar == 0) {
+                    listaPrimeiraImagem.add(img);
+                }
+            }
+        }
+        for (Produto p : listaProd) {
+            for (ImagemProduto img : listaPrimeiraImagem) {
+                if (p.getIdProd() == img.getIdProd()) {
+                    p.setCaminho("https://storage.cloud.google.com/imagedb/" + img.getNome());
+                }
+            }
+        }
+
+        request.setAttribute("listaProdutoAtt", listaProd);
+        request.setAttribute("listaImagensAtt", listaPrimeiraImagem);
+        request.getRequestDispatcher("/WEB-INF/Index.jsp")
+                .forward(request, response);
     }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
